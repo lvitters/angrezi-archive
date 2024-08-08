6f708586d7153478239176e0a24430e5282c462e
lvitters <luvit023@gmail.com>
2024-08-08 17:49:37 +0200

    basic slider, but isnt really working

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,24 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	let year = 2024;
+	$: yearPos = year - 2013;
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<div id="container">
+	<div id="year" style="left: {(yearPos/100*90)}%">{year}</div>
+	<input type="range" bind:value={year} min="2018" max="2118"/>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	#container {
+		display: block;
+		width: 100vw;
+	}
+	#year {
+		position: absolute;
+		top: 3%;
+	}
+	input {
+		margin-top: 1%;
+		width: 90%;
+	}
 </style>
