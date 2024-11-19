5d1ee6ddd3e569d86b0e33ea5c738623cb05969e
lvitters <luvit023@gmail.com>
2024-11-20 00:09:41 +0100

    fonts are working, choose random font for audiofiles!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import YearSlider from './yearSlider.svelte';
+	import '$lib/css/fonts.css';
 
 	let selectedYear = $state(0);
-	let audioFiles = $state([] as string[]);	//TODO: why does this need to be $state? selectedYear should trigger it, no?
+	let audioFiles = $state([] as string[]);	//TODO: why does this need to be $state? selectedYear should trigger the change, no?
 	let audioData: {[year: number]: string[] } = {};	//declare audioData object with array of strings for every year, init as empty
+
+	let fonts: string[] = ['Archivo_Narrow', 'Millimetre', 'savateregular', 'Sporting_Grotesque_Regular', 'terminal_grotesque', 'VG5000'];
 
 	//when a $state() changes
 	$effect(()=> {
 		loadAudioData(selectedYear);
+		chooseRandomFont();
 	})
 
 	//load the audio data
@@ -34,45 +38,51 @@
 			audioFiles = []; //No files for this year, clear the array
 		}
   	}
+
+	//choose a random font for each audioFile, insert as class name in divs
+	function chooseRandomFont() {
+		let font = fonts[Math.floor(Math.random() * fonts.length)];
+		//console.log(font);
+		return font;
+	}
 </script>
 
 <YearSlider bind:year={selectedYear}></YearSlider>
 
 <!-- display files -->
-<div class="audioFilesBox">
 	{#if audioFiles.length}
 		<ul class="audioFilesList">
 		{#each audioFiles as file}
-			<li>
-				<p class="audioFile">{file.replace("/audio/" + selectedYear + "/","").replace(".mp3", "")}</p>	<!-- remove path and file ending from name for display -->
-				<audio controls>
-					<source src={file} type="audio/mp3" />
+			<li class="audioFile">
+				<p id={chooseRandomFont()}>{file.replace("/audio/" + selectedYear + "/","").replace(".mp3", "")}</p>	<!-- remove path and file ending from name for display -->
+				<audio controls class="controls">
+					<source src={file} type="audio/mp3"/>
 					Your browser does not support the audio element.
 				</audio>
 			</li>
 		{/each}
 		</ul>
 	{:else}
-		<p>No audio files available for {selectedYear}.</p>
+		<p class={chooseRandomFont()}>No audio files available for {selectedYear}.</p>
 	{/if}
-</div>
 
 <style>
 
 	.audioFilesList {
-		display: flex-box;
-		list-style-type: none;
+		margin-top: 3rem;
+		list-style-type: none;  /* remove marker next to list item */
 	}
 
 	.audioFile {
-		/* font-family: 'Archivo Narrow'; */
-		/* font-family: 'Sporting Grotesque_Regular'; */
-		/* font-family: 'Sporting Grotesque_Bold'; */
-		/* font-family: 'savateregular'; */
-		/* font-family: 'VG5000'; */
-		font-family: Millimetre;
-		/* font-family: 'terminal_grotesque'; */
-		/* font-family: 'Roboto Regular'; */
+		display: flex;
+		margin-left: -2.5rem;
+		padding: .5rem;
+		border-style: solid;
+		border-width: 1px;
+	}
+
+	.controls {
+		margin-left: auto;
 	}
 
 </style>
