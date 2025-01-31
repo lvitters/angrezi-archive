b8fcca50523129e7e920305f048d5a9bbd29d3f1
lvitters <luvit023@gmail.com>
2025-01-31 15:06:12 +0100

    moved db stuff to folder outside "src" so it can be copied manually and hopefully updated on the server

--- db/drizzle.config.ts
+++ db/drizzle.config.ts
@@ -6,10 +6,10 @@ import type { Config } from "drizzle-kit";
 dotenv.config();
 
 export default {
-  schema: "./src/lib/server/db/schema.ts",
+  schema: "./db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DB_URL as string,
   },
-  out: "./src/lib/server/db/migrations",
+  out: "./db/migrations",
 } satisfies Config;
