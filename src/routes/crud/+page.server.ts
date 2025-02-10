5e335a46225a8797e2e1258a411c935b778aa18f
lvitters <luvit023@gmail.com>
2025-02-10 19:11:49 +0100

    add CRUD route, lets see if I can do it!

---
+++ src/routes/crud/+page.server.ts
@@ -0,0 +1,15 @@
+import { getAllEntries } from "../../../db/entries.js";
+//import type { PageServerLoad } from './$types'; //why don't I need this?
+
+//load from db with year provided, this is called a load function
+export const load = async ({}) => {
+	try {
+		// fetch files for the specified year
+		const audioFiles = await getAllEntries();
+
+		return { audioFiles }; // return data to the page
+	} catch (error) {
+		console.error("Database error:", error);
+		throw error;
+	}
+};
---
+++ src/routes/crud/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import YearSlider from "../yearSlider.svelte";
+	import AudioPlayer from "../audioPlayer.svelte";
+
+	// apparently this will automatically fetch all the data via the load function in +page.server.ts? this is why SSR is good (I hope it works)
+	let { data } = $props();
+	const { audioFiles } = data;
+</script>
+
+<!-- display files -->
+{#if audioFiles.length > 0}
+	<table class="m-2">
+		<thead>
+			<tr class="border border-black">
+				<th class="border border-black" scope="col">date</th>
+				<th class="border border-black p-4 whitespace-nowrap" scope="col">year folder</th>
+				<th class="border border-black p-4 whitespace-nowrap" scope="col">title</th>
+				<th
+					class="cursor-pointer p-4 whitespace-nowrap text-green-500 hover:bg-green-500 hover:text-white"
+					colspan="2"
+					scope="col">
+					add new
+				</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each audioFiles as audioFile (audioFile.id)}
+				<tr>
+					<td class="border border-black p-2 whitespace-nowrap">{audioFile.sortDate}</td>
+					<td class="border border-black p-2 whitespace-nowrap">{audioFile.year}</td>
+					<td class="border border-black p-2">{audioFile.title}</td>
+					<th
+						class="cursor-pointer border border-black p-2 whitespace-nowrap text-blue-500 hover:bg-blue-500 hover:text-white"
+						scope="col">
+						edit
+					</th>
+					<th
+						class="cursor-pointer border border-black p-2 whitespace-nowrap text-red-500 hover:bg-red-500 hover:text-white"
+						scope="col">
+						delete
+					</th>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{/if}
